6864a85f60ac9a9236baa71e8b83ad164c0fc0fa
yshysh <yinshanhui_work@163.com>
2018-01-06 19:32:01 +0800

    test

---
+++ script/update_po_file.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git_setup() {
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+}
+
+git_add() {
+    git add ../doc/locales
+    git commit --message "Update po files: $TRAVIS_BUILD_NUMBER"
+}
+
+git_push() {
+    git push --quiet test
+}
+
+git_setup
+git_add
+git_push
